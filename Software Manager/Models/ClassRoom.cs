a9c730107886a4f7bae022752c4488dfdaced540
Juhasz Marton <juhaszmarci2001@gmail.com>
2023-03-09 17:57:47 +0100

    Add project files.

---
+++ Software Manager/Models/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Software_Manager.Models
+{
+    public class ClassRoom
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        public string RoomNumber { get; set; }
+        [Required]
+        public double StorageCapacity { get; set; }
+
+        public virtual ICollection<SoftwareClaim> softwareClaims { get; set; }
+        public ClassRoom() {
+            softwareClaims = new HashSet<SoftwareClaim>();
+            Id =Guid.NewGuid().ToString();
+        
+        }
+    }
+}
