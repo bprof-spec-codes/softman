0802b1efef0674ac18d67c60c54d81b3b5aacabe
szabii70@stud.uni-obuda.hu <szabii70@stud.uni-obuda.hu>
2023-03-11 12:08:58 +0100

    ClassRoomController test datas

---
+++ SoftwareManagerAPI/Controllers/ClassController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareManagerAPI.Models;
+
+namespace SoftwareManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClassController : ControllerBase
+    {
+        static List<ClassRoom> ClassRooms = new List<ClassRoom>()
+        {
+            new ClassRoom(){Id="Class0000",RoomNumber="BA.01.11.",StorageCapacity=3000},
+            new ClassRoom(){Id="Class0001",RoomNumber="BC.04.07.",StorageCapacity=3500},
+            new ClassRoom(){Id="Class0002",RoomNumber="BA.11.20.",StorageCapacity=1400},
+            new ClassRoom(){Id="Class0003",RoomNumber="BA.03.18.",StorageCapacity=1000},
+            new ClassRoom(){Id="Class0004",RoomNumber="BD.02.11.",StorageCapacity=2000}
+        };
+    }
+}
---
+++ SoftwareManagerAPI/Controllers/SoftwareClaimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareManagerAPI.Models;
+
+namespace SoftwareManagerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SoftwareClaimController : ControllerBase
+    {
+
+    }
+}
