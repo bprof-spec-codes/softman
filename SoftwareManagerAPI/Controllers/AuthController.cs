11244198a33f4266dd0e44cf4e2180910f2c7758
Juhasz Marton <juhaszmarci2001@gmail.com>
2023-04-25 16:17:27 +0200

    facebook illetve google login (Tesztelés szükséges)

--- SoftwareManagerAPI/Controllers/AuthController.cs
+++ SoftwareManagerAPI/Controllers/AuthController.cs
@@ -24,27 +24,38 @@ namespace SoftwareManagerAPI.Controllers
             _userManager = userManager;
         }
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        private async Task<IActionResult> SocialAuth(AppUser user)
         {
-            var user = await _userManager.FindByNameAsync(model.UserName);
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            if (_userManager.Users.FirstOrDefault(t => t.Email == user.Email) == null)
             {
-                var claim = new List<Claim>
+                var res = await _userManager.CreateAsync(user);
+                if (res.Succeeded)
                 {
-                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-                    new Claim(JwtRegisteredClaimNames.Name, user.UserName),
-                    new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
-                };
-                foreach (var role in await _userManager.GetRolesAsync(user))
+                    await _userManager.AddToRoleAsync(user, "Customer");
+                }
+            }
+            var appuser = _userManager.Users.FirstOrDefault(t => t.Email == user.Email);
+            if (appuser != null)
+            {
+                var claim = new List<Claim> {
+          new Claim(JwtRegisteredClaimNames.Sub, appuser.UserName),
+          new Claim(JwtRegisteredClaimNames.NameId, appuser.UserName),
+          new Claim(JwtRegisteredClaimNames.Name, appuser.UserName)
+        };
+
+                foreach (var role in await _userManager.GetRolesAsync(appuser))
                 {
                     claim.Add(new Claim(ClaimTypes.Role, role));
                 }
-                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+            ("nagyonhosszutitkoskodhelye"));
                 var token = new JwtSecurityToken(
                  issuer: "http://www.security.org", audience: "http://www.security.org",
                  claims: claim, expires: DateTime.Now.AddMinutes(60),
-                 signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                 signingCredentials: new SigningCredentials
+            (signinKey, SecurityAlgorithms.HmacSha256)
                 );
+
                 return Ok(new
                 {
                     token = new JwtSecurityTokenHandler().WriteToken(token),
@@ -75,7 +86,7 @@ namespace SoftwareManagerAPI.Controllers
             {
                 throw new Exception("Nem felel meg az IdentityUser követelményeinek");
             }
-            await _userManager.AddToRoleAsync(user, "Admin");
+            await _userManager.AddToRoleAsync(user, "Customer");
 
             return Ok();
         }
@@ -102,5 +113,134 @@ namespace SoftwareManagerAPI.Controllers
             return Unauthorized();
 
         }
+
+        [Authorize]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMyself()
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.UserName == this.User.Identity.Name);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> Facebook([FromBody] SocialToken token)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://graph.facebook.com");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync($"me?fields=first_name,last_name,picture,email&access_token={token.Token}");
+            var result = await response.Content.ReadFromJsonAsync<FbModel>();
+            if (result != null)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = result.last_name,
+                    LastName = result.first_name,
+                    Email = result.email,
+                    UserName = result.email,
+                };
+
+                return await SocialAuth(user);
+            }
+            return Unauthorized();
+        }
+
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> Google([FromBody] SocialToken token)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://oauth2.googleapis.com");
+            client.DefaultRequestHeaders.Accept.Add(
+              new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync($"tokeninfo?id_token={token.Token}");
+            var result = await response.Content.ReadFromJsonAsync<GoogleModel>();
+
+            if (result != null)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = result.given_name,
+                    LastName = result.family_name,
+                    Email = result.email,
+                    UserName = result.email,
+                };
+                return await SocialAuth(user);
+            }
+            return Unauthorized();
+        }
+
+
+
+        
+
+
+
     }
+
+
+    internal class MsModel
+    {
+        public string given_name { get; set; }
+        public string family_name { get; set; }
+        public string email { get; set; }
+    }
+
+    public class SocialToken
+    {
+        public string Token { get; set; }
+    }
+
+    public class FbModel
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public Picture picture { get; set; }
+        public string email { get; set; }
+        public string id { get; set; }
+        public class Picture
+        {
+            public PictureData data { get; set; }
+            public class PictureData
+            {
+                public int height { get; set; }
+                public bool is_silhouette { get; set; }
+                public string url { get; set; }
+                public int width { get; set; }
+            }
+        }
+    }
+
+    public class GoogleModel
+    {
+        public string iss { get; set; }
+        public string azp { get; set; }
+        public string aud { get; set; }
+        public string sub { get; set; }
+        public string email { get; set; }
+        public string email_verified { get; set; }
+        public string at_hash { get; set; }
+        public string name { get; set; }
+        public string picture { get; set; }
+        public string given_name { get; set; }
+        public string family_name { get; set; }
+        public string locale { get; set; }
+        public string iat { get; set; }
+        public string exp { get; set; }
+        public string jti { get; set; }
+        public string alg { get; set; }
+        public string kid { get; set; }
+        public string typ { get; set; }
+    }
+
+
+
 }
+
