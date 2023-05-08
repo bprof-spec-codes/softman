d61fb4c5c8577a38fcf395b063a56bfc872526c7
Juhasz Marton <juhaszmarci2001@gmail.com>
2023-05-08 19:58:46 +0200

    authentication,login fix

--- SoftwareManagerAPI/Controllers/AuthController.cs
+++ SoftwareManagerAPI/Controllers/AuthController.cs
@@ -19,6 +19,38 @@ namespace SoftwareManagerAPI.Controllers
 
         private readonly UserManager<AppUser> _userManager;
 
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new List<Claim>
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+                };
+                foreach (var role in await _userManager.GetRolesAsync(user))
+                {
+                    claim.Add(new Claim(ClaimTypes.Role, role));
+                }
+                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
+                var token = new JwtSecurityToken(
+                 issuer: "http://www.security.org", audience: "http://www.security.org",
+                 claims: claim, expires: DateTime.Now.AddMinutes(60),
+                 signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
+        }
+
+
         public AuthController(UserManager<AppUser> userManager, IConfiguration configuration)
         {
             _userManager = userManager;
