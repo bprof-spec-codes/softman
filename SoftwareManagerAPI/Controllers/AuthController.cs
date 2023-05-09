1d49f747f0ca7a629496466dc41b6eb9e281df93
Schäffer Tamás <SchafferTamas@stud.uni-obuda.hu>
2023-05-09 02:31:53 +0200

    remove my changes in auth

--- SoftwareManagerAPI/Controllers/AuthController.cs
+++ SoftwareManagerAPI/Controllers/AuthController.cs
@@ -99,32 +99,6 @@ namespace SoftwareManagerAPI.Controllers
             return Unauthorized();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
-        {
-            var user = await _userManager.FindByNameAsync(model.UserName);
-            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
-            {
-                var claim = new List<Claim> { new Claim(JwtRegisteredClaimNames.Sub, user.UserName) };
-                foreach (var role in await _userManager.GetRolesAsync(user))
-                {
-                    claim.Add(new Claim(ClaimTypes.Role, role));
-                }
-                var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
-                var token = new JwtSecurityToken(
-                issuer: "http://www.security.org", audience: "http://www.security.org",
-                claims: claim, expires: DateTime.Now.AddMinutes(60),
-                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
-                );
-                return Ok(new
-                {
-                    token = new JwtSecurityTokenHandler().WriteToken(token),
-                    expiration = token.ValidTo
-                });
-            }
-            return Unauthorized();
-        }
-
 
         [HttpPut]
         public async Task<IActionResult> InsertUser([FromBody] RegisterViewModel model)
