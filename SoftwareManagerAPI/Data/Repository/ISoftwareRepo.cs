71b0fe2bc23a57081e1c608752ea44722b954f46
Hubor-Hunor <xsurawp@gmail.com>
2023-05-17 14:04:27 +0200

    Repo changes

--- SoftwareManagerAPI/Data/Repository/ISoftwareRepo.cs
+++ SoftwareManagerAPI/Data/Repository/ISoftwareRepo.cs
@@ -1,10 +1,12 @@
 ﻿using SoftwareManagerAPI.Models;
+using SoftwareManagerAPI.Models.ViewModels;
 
 namespace SoftwareManagerAPI.Data.Repository
 {
     public interface ISoftwareRepo
     {
         Software Create(Software Software);
+        Software CreateSoftware(SoftwareViewModel Software);
         Software DeleteByID(string id);
         IEnumerable<Software> ReadAll();
         Software ReadByID(string id);
