4192a30916238e7eddb0235d4c158f38184aef6e
Hubor-Hunor <xsurawp@gmail.com>
2023-05-17 14:04:49 +0200

    SoftwareViewModel created

---
+++ SoftwareManagerAPI/Models/ViewModels/SoftwareViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftwareManagerAPI.Models.ViewModels
+{
+    public class SoftwareViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string VersionNumber { get; set; }
+        [Required]
+        public double Size { get; set; }
+        public IFormFile imageFile { get; set; }
+    }
+}
